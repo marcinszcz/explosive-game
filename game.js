ceeb6131b878decba0064603c4051ac8fa92524e
marcinszcz <mrs2vip@gmail.com>
2017-03-19 16:54:36 +0100

    repair bugs and add new features

--- game.js
+++ game.js
@@ -15,13 +15,6 @@ function requestAnimationFrame (callback) {
 */
 context.scale(20, 20)
 
-const matrix = [
-  [0, 0, 0],
-  [1, 1, 1],
-  [0, 1, 0]
-
-]
-
 function collide (arena, player) {
   const [m, o] = [player.matrix, player.position]
   for (let y = 0; y < m.length; y++) {
@@ -79,9 +72,62 @@ function playerDrop () {
   if (collide(arena, player)) {
     player.position.y--
     merge(arena, player)
-    player.position.y = 0
+    playerReset()
   }
   dropCounter = 0
+}
+
+function playerMove(direction) {
+  player.position.x += direction
+  if (collide(arena, player)) {
+    player.position.x -= direction
+  }
+}
+
+function playerReset() {
+  const pieces = 'ILJOTSZ'
+  player.matrix = createPiece(pieces.anchor[pieces.length * Math.random() | 0])
+  player.position.y = 0
+  player.position.x = (arena[0].length / 2 | 0) - 
+  (player.matirx[0].length /2 | 0)
+}
+
+function playerRotate(direction) {
+  let offset = 1
+  rotate(player.matrix, direction)
+  while (collide(arena, player)) {
+      const position = player.position.x
+      player.position.x += offset
+      offset = -(offset = (offset > 0 ? 1 : -1))
+      if (offset > player.matrix[0].length) {
+        rotate(player.matrix, -direction)
+        player.position.x = position
+        return 
+      }
+  }
+
+}
+
+function rotate(matrix, direction) {
+  for (let y =0; y< matrix.length; y++){
+    for (let x = 0; x < y; x++){
+      {
+        [
+          matrix[x][y],
+          matrix[y][x]
+        ] = [
+          matrix[y][x],
+          matrix[x][y]
+        ]
+      }
+    }
+    if (direction > 0) {
+      matrix.forEach(row => row.reverse())
+    } else {
+      matrix.reverse()
+    }
+  }
+
 }
 
 let dropCounter = 0
@@ -103,23 +149,73 @@ function update (time = 0) {
 const arena = createMatrix(12, 20)
 // console.log(arena)
 
+// only for tests
+// const items = ['T', 'S1', 'S2', 'O', 'I', 'L1', 'L2']
+// let item = items[Math.floor(Math.random()*items.length)]
+
 const player = {
   position: {x: 5, y: 5},
-  matrix: matrix
+  matrix: createPiece(item)
+}
+
+function createPiece(type) {
+  if (type === 'T') {
+    return [
+      [1, 0, 0],
+      [1, 1, 0],
+      [1, 0, 0]
+]
+  } else if (type === 'S') {
+      return [
+        [1, 0, 0],
+        [1, 1, 0],
+        [0, 1, 0]
+]
+  } else if (type === 'Z') {
+      return [
+        [0, 0, 1],
+        [0, 1, 1],
+        [0, 1, 0]
+]
+  } else if (type === 'O') {
+      return [
+        [1, 1],
+        [1, 1],
+        ]
+  } else if (type === 'I') {
+      return [
+        [0, 1, 0],
+        [0, 1, 0],
+        [0, 1, 0],
+        [0, 1, 0]
+]
+  } else if (type === 'L') {
+      return [
+        [0, 1, 1],
+        [0, 1, 0],
+        [0, 1, 0]
+]
+  } else if (type === 'J') {
+      return [
+        [1, 1, 0],
+        [0, 1, 0],
+        [0, 1, 0]
+]
+  }
 }
 
 document.addEventListener('keydown', event => {
   if (event.keyCode === 37) {
-    player.position.x--
+    playerMove(-1)
   } else if (event.keyCode === 39) {
-    player.position.x++
+    playerMove(1)
   } else if (event.keyCode === 40) {
     playerDrop()
+  } else if (event.keyCode === 81) {
+    playerRotate(-1)
+  } else if (event.keyCode === 87) {
+    playerRotate(1)
   }
 })
 
 update()
-
-
-
-
